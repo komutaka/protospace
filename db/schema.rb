4ed8fb7d23dec165f7dc91dc30f6eac1b8c50469
Kiyotaka Komugi <komutaka8@gmail.com>
2016-03-22 22:39:57 +0900

    投稿される作品がマイページに表示される必要があるため、productモデルを作成し、データベースの設計を行った。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312131452) do
+ActiveRecord::Schema.define(version: 20160322132406) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "catch_copy", limit: 255
+    t.string   "consept",    limit: 255
+    t.string   "image_url",  limit: 255
+    t.string   "user_id",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "nickname",               limit: 255
