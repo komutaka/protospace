4bb1c231560cea83a210e0b64302c882eb0aaa17
Kiyotaka Komugi <komutaka8@gmail.com>
2016-03-24 16:37:12 +0900

    編集機能の実装にdeviseを用いたため、不要になり削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def edit
-  end
-
-  def update
-    current_user.update(update_params)
-  end
-
   private
   def update_params
     params.require(:user)
