06cb0b83a076cfa0d2e2381636a9a5bae510ca96
Kiyotaka Komugi <komutaka8@gmail.com>
2016-03-25 19:50:09 +0900

    不要な記述であったため削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  def after_sign_out_path_for(resource)
-    root_path
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:nickname, :member, :work, :profile, :avatar)
