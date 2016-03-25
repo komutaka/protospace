7db262cff3d2e5ec2c2f943bed2a7d33af0d624f
Kiyotaka Komugi <komutaka8@gmail.com>
2016-03-25 22:07:36 +0900

    userコントローラーにてedit, updateアクションを定義し、パスを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    current_user.update(update_params)
+    redirect_to action: :show
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:nickname, :member, :work, :profile, :avatar, :email)
+  end
 end
