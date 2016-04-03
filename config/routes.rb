590c0a264eb76be4be1ed2421b12e890f3d0c280
Kiyotaka Komugi <komutaka8@gmail.com>
2016-04-03 16:13:12 +0900

    createアクションではなく、newアクションへのルートに修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-  resources :projects, only: [:create]
+  resources :projects, only: [:new]
   root to: "projects#index"
 end
