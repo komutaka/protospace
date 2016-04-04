a5dd807b7815ef55bebdfbc6e3f466a711c01b4b
Kiyotaka Komugi <komutaka8@gmail.com>
2016-04-04 17:00:24 +0900

    投稿に必要なアクションを定義した

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-  resources :projects, only: [:new]
+  resources :projects, only: [:new, :create]
   root to: "projects#index"
 end
