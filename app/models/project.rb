78c2d8d6044175676917f0c0fa8879a03ddbf63f
Kiyotaka Komugi <komutaka8@gmail.com>
2016-04-04 22:38:27 +0900

    複数の子レコードを作成するためにaccepts_nested_attributes_forを定義した

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   belongs_to :user
-
+  has_many :images
+  accepts_nested_attributes_for :images
   validates_presence_of :user_id, :image
 end
