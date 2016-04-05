475793cce44ac870f630b5a75202d1413fa6ae70
Kiyotaka Komugi <komutaka8@gmail.com>
2016-04-05 14:44:53 +0900

    imageモデルに紐づけるためにbuildメソッドを使用。 エラーメッセージを出す際にコントローラーを経由したくないため、redirect...ではなくrenderを使用。 user_idが欲しいため、current_user.idに変更

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,21 +4,20 @@ class ProjectsController < ApplicationController
 
   def new
     @project = Project.new
+    @project.images.build
   end
 
   def create
     @project = Project.new(project_params)
-    @project.images.build
     if @project.save
-      binding.pry
       redirect_to action: :index, notice: '投稿が完了しました'
     else
-      redirect_to action: :new, notice: '投稿に失敗しました'
+      render :new, notice: '投稿に失敗しました'
     end
   end
 
   private
   def project_params
-    params.require(:project).permit(:catch_copy, :title, :concept, images_attributes: [:id, :image, :status]).merge(user_id: current_user)
+    params.require(:project).permit(:catch_copy, :title, :concept, images_attributes: [:id, :image, :status]).merge(user_id: current_user.id)
   end
 end
