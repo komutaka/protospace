141f317c2131928984e0d87437fe6758ee9b2233
Kiyotaka Komugi <komutaka8@gmail.com>
2016-04-05 13:22:09 +0900

    newアクションとcreateアクションを定義した。

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,24 @@
 class ProjectsController < ApplicationController
   def index
-    @project = Project.new
   end
 
   def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.images.build
+    if @project.save
+      binding.pry
+      redirect_to action: :index, notice: '投稿が完了しました'
+    else
+      redirect_to action: :new, notice: '投稿に失敗しました'
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:catch_copy, :title, :concept, images_attributes: [:id, :image, :status]).merge(user_id: current_user)
   end
 end
