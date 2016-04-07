881e312cf907a3a41257d683232b3a2eacf7b784
Kiyotaka Komugi <komutaka8@gmail.com>
2016-04-07 10:57:21 +0900

    project-imageの親子関係にあるモデルにおいて親のレコードを削除したときに子も一緒に削除したいため記述を追加

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :images
+  dependent: :delete_all
   accepts_nested_attributes_for :images, reject_if: :reject_images
   validates_presence_of :title, :catch_copy, :concept
 
