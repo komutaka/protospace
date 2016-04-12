51c11b61473bdcda39c9f6c872cc31a8715665ea
Kiyotaka Komugi <komutaka8@gmail.com>
2016-04-12 21:35:53 +0900

    projectのshowページを作るためにルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-  resources :projects, only: [:new, :create]
+  resources :projects, only: [:new, :create, :show]
   root to: "projects#index"
 end
