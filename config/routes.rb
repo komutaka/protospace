f202297d970287cbf2cfd590386b6cfa75f1ef22
Kiyotaka Komugi <komutaka8@gmail.com>
2016-04-13 16:40:19 +0900

    newest, rankingに対するルーティングを行った上で、root toするviewを変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   resources :projects, only: [:new, :create, :show]
-  root to: "projects#index"
+  resources :projects do
+    resources :newest, :ranking, module: 'projects'
+  end
+  root to: "projects/ranking#index"
 end
