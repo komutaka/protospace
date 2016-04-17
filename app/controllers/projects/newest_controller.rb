e9fdf409f43a4f205da3b1d338ef1012a3ef53f4
Kiyotaka Komugi <komutaka8@gmail.com>
2016-04-17 09:39:43 +0900

    投稿の最新順に並ぶように記述を変更

--- app/controllers/projects/newest_controller.rb
+++ app/controllers/projects/newest_controller.rb
@@ -1,5 +1,5 @@
 class Projects::NewestController < ApplicationController
   def index
-    @projects = Project.includes(:user).page(params[:page]).per(8).order("id: :desc")
+    @projects = Project.includes(:user).page(params[:page]).per(8).order("created_at DESC")
   end
 end
