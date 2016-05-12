a666da9cd698fa348d97e8c0f8ce68381778bf0d
Kiyotaka Komugi <komutaka8@gmail.com>
2016-05-12 15:41:50 +0900

    Set routing about tag.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
+  resources :tags, only: [:index, :show]
+
   namespace :projects do
     resources :newest, only: :index
     resources :ranking, only: :index
   end
+
   resources :projects, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :likes, only: [:create, :destroy], module: :projects
     resources :comments, only: :create, module: :projects
   end
+
   root to: "projects/ranking#index"
 end
