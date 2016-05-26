a902952ec2937e0dd7423078be534b5792989cb4
Kiyotaka Komugi <komutaka8@gmail.com>
2016-05-26 16:31:23 +0900

    Import the plugin jQuery Upload Thumbs.

---
+++ app/assets/javascripts/image.js
@@ -0,0 +1,20 @@
+$(function() {
+
+  $('#new-main-image input:file').uploadThumbs({
+  });
+  $('#new-sub1-image input:file').uploadThumbs({
+  });
+  $('#new-sub2-image input:file').uploadThumbs({
+  });
+  $('#edit-main-image input:file').uploadThumbs({
+  });
+  $('#edit-sub1-image input:file').uploadThumbs({
+  });
+  $('#edit-sub2-image input:file').uploadThumbs({
+  });
+  $('#new-user-image input:file').uploadThumbs({
+  });
+  $('#edit-user-image input:file').uploadThumbs({
+  });
+
+});
