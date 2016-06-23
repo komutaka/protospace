3001332b3ce5de23e3e628fa8f6b50d9b4d24068
Kiyotaka Komugi <komutaka8@gmail.com>
2016-06-23 11:30:32 +0900

    Modify the statements about test of user.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,44 @@
 require 'rails_helper'
+
 describe User do
   describe '#create' do
-    it "is valid with a nickname, member, work, profile, avatar, email, password, password_confirmation" do
-      user = build(:user)
-      expect(user).to be_valid
+    context 'valid' do
+      it "is valid with all information" do
+        expect(build(:user)).to be_valid
+      end
     end
 
-    it "is invalid without a email" do
-      user = build(:user, email: "")
-      user.valid?
-      expect(user.errors[:email]).to include("を入力してください。")
-    end
+    context 'invalid' do
+      it "is invalid without an email" do
+        user = build(:user, email: "")
+        user.valid?
+        expect(user.errors[:email]).to include("を入力してください。")
+      end
 
-    it "is invalid without a password" do
-      user = build(:user, password: "")
-      user.valid?
-      expect(user.errors[:password]).to include("を入力してください。")
-    end
+      it "is invalid without a password" do
+        user = build(:user, password: "")
+        user.valid?
+        expect(user.errors[:password]).to include("を入力してください。")
+      end
 
-    it "is invalid without a password_confirmation" do
-      user = build(:user, password_confirmation: "")
-      user.valid?
-      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません。")
-    end
+      it "is invalid if password doesn't match password_confirmation" do
+        user = build(:user, password_confirmation: "hogehoge")
+        user.valid?
+        expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません。")
+      end
 
-    it "is invalid with a duplicate email address" do
-      user = create(:user)
-      another_user = build(:user, email: user.email)
-      another_user.valid?
-      expect(another_user.errors[:email]).to include("はすでに存在します。")
-    end
+      it "is invalid with a duplicate email address" do
+        user = create(:user)
+        another_user = build(:user, email: user.email)
+        another_user.valid?
+        expect(another_user.errors[:email]).to include("はすでに存在します。")
+      end
 
-    it "is invalid with a password that has less than 7 characters " do
-      user = build(:user, password: "0000000", password_confirmation: "0000000")
-      user.valid?
-      expect(user.errors[:password][0]).to include("は8文字以上で入力してください。")
+      it "is invalid with a password that has less than 7 characters " do
+        user = build(:user, password: "0000000", password_confirmation: "0000000")
+        user.valid?
+        expect(user.errors[:password][0]).to include("は8文字以上で入力してください。")
+      end
     end
 
   end
