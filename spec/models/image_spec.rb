34c1fc7763d210d3d945a0e7abda50d9b63ea249
Kiyotaka Komugi <komutaka8@gmail.com>
2016-06-23 09:53:13 +0900

    Set the test of image-model.

---
+++ spec/models/image_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+# describe Image do
+#   describe '#create' do
+#     context "with valid params" do
+#       it "creates a new Project" do
+#         params = { images_attributes: [ FactoryGirl.attributes_for( :image ) ] }
+#         expect {
+#           post :create,
+#           project: FactoryGirl.attributes_for( :project ).merge( params )
+#           }.to change( Project, :count ).by( 1 ).and change( Image, :count ).by( 1 )
+#        end
+#     end
+#   end
+# end
