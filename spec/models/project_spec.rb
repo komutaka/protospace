8fde8f2386b705183555bf1c7ed07004f205af7b
Kiyotaka Komugi <komutaka8@gmail.com>
2016-06-23 11:30:05 +0900

    Modify the statements about test of project-model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,35 +1,41 @@
 require 'rails_helper'
-describe Project do
-  describe '#create' do
-    it "is valid with a catch_copy, user_id, title, concept" do
-      project = build(:project)
-      expect(project).to be_valid
-    end
 
-    describe 'associations' do
-      it 'is associated with a user' do
-        user = create(:user)
-        project = create(:project, user: user)
-        expect(project.user).to eq user
+describe Project do
+
+  describe 'associations' do
+    it 'is associated with a user' do
+      user = create(:user)
+      project = create(:project, user: user)
+      expect(project.user).to eq user
+    end
+  end
+
+  describe '#create' do
+    context 'valid' do
+      it "is valid with all information" do
+        project = build(:project)
+        expect(project).to be_valid
       end
     end
 
-    it "is invalid without a catch_copy" do
-      project = build(:project, catch_copy: "")
-      project.valid?
-      expect(project.errors[:catch_copy]).to include("を入力してください。")
-    end
+    context 'invalid' do
+      it "is invalid without a catch_copy" do
+        project = build(:project, catch_copy: "")
+        project.valid?
+        expect(project.errors[:catch_copy]).to include("を入力してください。")
+      end
 
-    it "is invalid without a title" do
-      project = build(:project, title: "")
-      project.valid?
-      expect(project.errors[:title]).to include("を入力してください。")
-    end
+      it "is invalid without a title" do
+        project = build(:project, title: "")
+        project.valid?
+        expect(project.errors[:title]).to include("を入力してください。")
+      end
 
-    it "is invalid without a concept" do
-      project = build(:project, concept: "")
-      project.valid?
-      expect(project.errors[:concept]).to include("を入力してください。")
+      it "is invalid without a concept" do
+        project = build(:project, concept: "")
+        project.valid?
+        expect(project.errors[:concept]).to include("を入力してください。")
+      end
     end
 
   end
