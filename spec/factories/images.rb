b16ee9f9761ae47e417484c8b679b6d98082d9bd
Kiyotaka Komugi <komutaka8@gmail.com>
2016-06-24 22:19:03 +0900

    Set the dummy data of image.

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
 
   factory :image do
-    project   nil
+    status 0
+    project_id  project
+    image { fixture_file_upload("spec/fixtures/enkare.jpg", "image/jpg") }
   end
 
 end
